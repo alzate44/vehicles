fbe026516f75f9383c7bf4e3bf24241e90f25a87
Alejandro Alzate <alejo.15975355@gmail.com>
2021-08-28 10:57:57 -0500

    Add VehicleType and Create DataBase

---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+    // Entidad VehicleType (Tabla), lleva la DataNotation
+    public class VehicleType
+    {
+        public int Id { get; set; } //table Autoincrementing
+
+        [Display(Name = "Tipo de vehículo")] 
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo  {0} es obligatorio.")]  // Description obligatoria 
+        public string Description { get; set; }
+    }
+}
